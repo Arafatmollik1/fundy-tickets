58175fb09256a1a619d75bc1247a383d325f3054
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2024-02-25 18:40:15 +0200

    optimise tickets view

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <!-- Scripts -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.5/dist/cdn.min.js"></script>
     @vite(["resources/css/app.css", "resources/js/app.js"])
 </head>
 <body class="bg-fundy-main text-fundy-text">
--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,19 +1,23 @@
 <x-layouts.app>
     <div class="container mx-auto flex justify-center h-screen mt-10 mb-10">
-        <div class="flex flex-col justify-evenly w-[300px] lg:w-full">
-            <div class="flex flex-col lg:flex-row shadow-lg rounded">
+        <div class="flex flex-col bg-transparent justify-evenly w-[300px] lg:w-full">
+            <div class="flex flex-col bg-white lg:flex-row shadow-lg rounded">
                 <div class="w-full">
                     <img class="w-full h-full object-cover rounded-t lg:rounded"
                          src="https://images.unsplash.com/photo-1640955011254-39734e60b16f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                          alt="laser tag game">
                 </div>
                 <div class="p-4">
-                    <h1 class="text-xl font-bold text-left my-4">MEGAZONE LASER TAG GAME</h1>
-                    <p class="text-left mb-4">Laser tag is played in a dimly lit, fog-filled, multi-level arena where
+                    <h1 class="text-xl font-bold text-left my-4">
+                        MEGAZONE LASER TAG GAME
+                    </h1>
+                    <p class="text-left mb-4">
+                        Laser tag is played in a dimly lit, fog-filled, multi-level arena where
                         players are
                         provided with high tech vests and awesome but harmless laser guns. Players then try to score
                         points
-                        by deactivating other players’ vests and destroying their bases.</p>
+                        by deactivating other players’ vests and destroying their bases.
+                    </p>
                     <div class="flex flex-col lg:flex-row pb-6 gap-2">
                         <div
                             class="w-fit bg-gray-100 text-gray-800 text-xs font-medium me-2 p-2 rounded-full"
@@ -35,7 +39,8 @@
                         </div>
                     </div>
                     <button
-                        class="bg-fundy-primary hover:bg-fundy-hover-primary text-white font-bold py-2 px-4 rounded">
+                        class="bg-fundy-primary hover:bg-fundy-hover-primary text-white font-bold py-2 px-4 rounded"
+                    >
                         Book Now
                     </button>
                 </div>
